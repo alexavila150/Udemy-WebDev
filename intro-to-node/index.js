177f5f34efa355d8603717390e7e125bafdfef8f
alexavila150 <alexadrianavila@gmail.com>
2020-06-15 19:55:29 -0600

    intro to node

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+let superheroes = require("superheroes");
+let supervillains = require("supervillains");
+
+let mySuperHeroName = superheroes.random();
+let mySuperVillainName = supervillains.random();
+
+
+console.log(mySuperHeroName + " VS " + mySuperVillainName);
